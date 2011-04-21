70129a14b5af0292ed9b836ed92200a76980b162
Kirby C. Bohling <kirby.bohling@gmail.com>
2011-04-21 05:38:46 -0500

    Save generated archetype after tweaking to use JUnit4.

---
+++ src/test/java/com/blogspot/codinghardknocks/AppTest.java
@@ -0,0 +1,20 @@
+package com.blogspot.codinghardknocks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue(true);
+    }
+}
